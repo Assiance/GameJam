89378b586f4d75335087ffbe0a8d0af8fe6f6764
Bradford Foxworth-Hill <bradford.foxworthhill@gmail.com>
2015-01-23 21:38:42 -0500

    pushed some project structure stuff

---
+++ Assets/GameJam/Scripts/Regular/General/MyMonoBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.GameJam.Scripts.Regular.General
+{
+    public abstract class MyMonoBehaviour : MonoBehaviour
+    {
+        private Transform _cachedTransform;
+
+        protected Transform CachedTransform
+        {
+            get
+            {
+                if (_cachedTransform == null)
+                    _cachedTransform = GetComponent<Transform>();
+
+                return _cachedTransform;
+            }
+        }
+    }
+}
