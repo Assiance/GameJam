f3f120ccc5f45a00d4bef94bfecefa6934d475e3
mrr0430 <marlonruiz@alumni.ecu.edu>
2015-01-24 15:16:29 -0500

    Networking server setup.

---
+++ Assets/GameJam/Scripts/Regular/NetworkManager.cs
@@ -0,0 +1,36 @@
+﻿using Assets.GameJam.Scripts.Regular.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.GameJam.Scripts.Regular
+{
+
+    public class NetworkManager : MyMonoBehaviour
+    {
+
+        private const string typeName = "UniqueGameName";
+         private const string gameName = "RoomName";
+            // launch the server
+            public void LaunchServer()
+            {
+                // Start a server that enables NAT punchthrough,
+                // listens on port 25005,
+                // and allows 8 clients to connect
+                if (!Network.isClient && !Network.isServer)
+                {
+                    Network.InitializeServer(4, 25005, true);
+                    MasterServer.RegisterHost(typeName, gameName);
+                }
+            }
+            // called when the server has been initialized
+            void OnServerInitialized()
+            {
+                Debug.Log("Server initialized");
+            }
+        
+    }
+}
---
+++ Assets/GameJam/Scripts/Regular/NetworkingMasterServer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.GameJam.Scripts.Regular.General;
+
+public class NetworkingMasterServer : MyMonoBehaviour
+{
+    // Assuming Master Server and Facilitator are on the samemachine
+    public string MasterServerIP = "127.0.0.1";
+    void Awake()
+    {
+        // set the IP and port of the Master Server to connect to
+        MasterServer.ipAddress = MasterServerIP;
+        MasterServer.port = 23466;
+        // set the IP and port of the Facilitator to connect to
+        Network.natFacilitatorIP = MasterServerIP;
+        Network.natFacilitatorPort = 50005;
+    }
+}
+
