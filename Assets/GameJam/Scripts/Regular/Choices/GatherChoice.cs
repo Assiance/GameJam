5fd29e985c812e0d168b15ae7d0d68ee74b5eb68
mrr0430 <marlonruiz@alumni.ecu.edu>
2015-01-24 01:24:49 -0500

    pushing player Interfaces

---
+++ Assets/GameJam/Scripts/Regular/Choices/GatherChoice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.GameJam.Scripts.Regular;
+
+namespace Assets.GameJam.Scripts
+{
+    public class GatherChoice : IChoice
+    {
+        public void Execute()
+        {
+            // Increase resources! Hooray!
+        }
+
+        public string Name
+        {
+            get { return "Gather"; }
+        }
+
+        public bool Disabled { get; set; }        
+    }
+}
---
+++ Assets/GameJam/Scripts/Regular/IChoice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.GameJam.Scripts.Regular
+{
+    public interface IChoice
+    {
+
+        bool Disabled {get;set;}
+        void Execute();
+        string Name {get;}
+    }
+}
